140e7d2b6b4c2af18e1850d149c4ab2c8464d393
renatagarza <renata.alvarezgr@uanl.edu.mx>
2025-09-23 19:23:27 -0600

    Add files via upload

---
+++ PowerShell-Ciberseguridad/Ejercicio2/inventario.ps1
@@ -0,0 +1,20 @@
+$edad = 25
+[float]$temperatura = 36.6
+$pi = 3.14159265358979
+$esEstudiante = $true
+$nombre = "Alex"
+$nada = $null
+Set-Variable -name "pais" -value "México"
+Write-Output $edad
+Write-Output $temperatura
+Write-Output $esEstudiante
+Write-Output $nombre
+Write-Output $nada
+Write-Output $pais
+$edad.GetType().Name
+$temperatura.GetType().Name
+$pi.GetType().Name
+$esEstudiante.GetType().Name
+$nombre.GetType().Name
+$nada.GetType().Name
+$pais.GetType().Name
